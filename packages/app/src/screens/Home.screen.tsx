7b0cc9edaa3f65a8b4d189a1c2bee731cd7635c3
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-05-22 17:45:27 +0000

    Feat: Implement most basic form of state management
    - Create a new package named `store` for state management.
    - Add the basic counter example from redux toolkit website to see if the
      basic code works.
    - Introduce `store` as a dependency in `packages/app`, `apps/web`, and
      `apps/android`.

--- packages/app/src/screens/Home.screen.tsx
+++ packages/app/src/screens/Home.screen.tsx
@@ -2,10 +2,20 @@ import CustomButton from "ui/src/CustomButton";
 import CustomHeader from "ui/src/CustomHeader";
 import CustomInput from "ui/src/CustomInput";
 import { useRouter } from "solito/router";
-import { Spacer, YStack, XStack, Button } from "ui";
+import { Spacer, YStack, XStack, Button, Text } from "ui";
+import {
+  useDispatch,
+  useSelector,
+  selectCount,
+  increment,
+  decrement,
+} from "store/src/index";
 
 const HomeScreen = () => {
   const { push } = useRouter();
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
   return (
     <YStack>
       <CustomHeader>Hi Hello Howdy!</CustomHeader>
@@ -28,6 +38,22 @@ const HomeScreen = () => {
       >
         Decade Stats
       </Button>
+      <Spacer />
+      <YStack>
+        <Button
+          // aria-label="Increment value"
+          onPress={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <Text>Count: {count}</Text>
+        <Button
+          // aria-label="Decrement value"
+          onPress={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </YStack>
     </YStack>
   );
 };
